01d2f8ddead2d221516ff09f1171b517716e98a0
dohoangkhiem <dohoangkhiem@gmail.com>
2012-04-17 12:33:30 +0700

    add hive-exp

---
+++ hive-experiment/src/main/java/khiem/dataprj/App.java
@@ -0,0 +1,13 @@
+package khiem.dataprj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
