f2a428ae9a973d24b14249f417afb86cfab30565
dohoangkhiem <dohoangkhiem@gmail.com>
2012-04-23 17:26:55 +0700

    init plfdemo

---
+++ plfdemo/src/main/java/khiem/dataprj/sampleplf/App.java
@@ -0,0 +1,13 @@
+package khiem.dataprj.sampleplf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
