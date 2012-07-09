1d7a69beaeedbbd5e4e3575f02e7f075c085486e
dohoangkhiem <dohoangkhiem@gmail.com>
2012-07-09 12:17:04 +0700

    Remove obsolete files

--- bouncingdata/src/main/webapp/resources/js/bouncingdata/dashboard.js
+++ bouncingdata/src/main/webapp/resources/js/bouncingdata/dashboard.js
@@ -2,3 +2,16 @@ function Dashboard() {
   
 }
 
+Dashboard.prototype.init = function() {
+  
+}
+
+Dashboard.prototype.addViz = function() {
+  
+}
+
+Dashboard.prototype.refresh = function() {
+  
+}
+
+com.bouncingdata.Dashboard = new Dashboard();
